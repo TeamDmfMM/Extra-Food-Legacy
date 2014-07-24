54669330236a113321cae83b82f2b2b963d4c417
dmf444 <becat444@gmail.com>
2014-07-23 23:33:23 -0400

    Updated

--- src/main/java/dmf444/ExtraFood/Client/renderer/BerryRender.java
+++ src/main/java/dmf444/ExtraFood/Client/renderer/BerryRender.java
@@ -1,7 +1,10 @@
 package dmf444.ExtraFood.Client.renderer;
 
 
+
+
 import org.lwjgl.opengl.GL11;
+
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
@@ -12,10 +15,16 @@ import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 
+
+
 public class BerryRender implements ISimpleBlockRenderingHandler {
 
 
+
+
 	static int myRenderID;
+
+
 
 
 	public BerryRender() {
@@ -24,13 +33,21 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 	}
 
 
+
+
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
 		Tessellator tessellator = Tessellator.instance;
 
 
+
+
 		    // if you don't perform this translation, the item won't sit in the player's hand properly in 3rd person view
-		    GL11.glTranslatef(-0.8F, -0.8F, -0.8F);
+		    GL11.glTranslatef(-0.3F, -0.3F, -0.3F);
+		    GL11.glDisable(GL11.GL_CULL_FACE);
+		    GL11.glDisable(GL11.GL_LIGHTING);
+		    GL11.glEnable(GL11.GL_ALPHA_TEST);
+
 
 
 		    // for "inventory" blocks (actually for items which are equipped, dropped, or in inventory), should render in [0,0,0] to [1,1,1]
@@ -38,12 +55,22 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 		    renderBlock(tessellator, 0.0, 0.0, 0.0, block);
 		    tessellator.draw();
 
+
+
+
 		    // don't forget to undo the translation you made at the start
-		    GL11.glTranslatef(0.8F, 0.8F, 0.8F);
+		    GL11.glTranslatef(0.3F, 0.3F, 0.3F);
+		    GL11.glEnable(GL11.GL_CULL_FACE);
+		    GL11.glEnable(GL11.GL_LIGHTING);
+		    GL11.glDisable(GL11.GL_ALPHA_TEST);
+
+
 
 
 
 	}
+
+
 
 
 	@Override
@@ -52,11 +79,17 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 		// TODO Auto-generated method stub
 
 
+
+
 		/*Tessellator tessellator = Tessellator.instance;
+
+
 
 
 	    // world blocks should render in [x,y,z] to [x+1, y+1, z+1]
 	    //     tessellator.startDrawingQuads() has already been called by the caller
+
+
 
 
 	    int lightValue = block.getMixedBrightnessForBlock(world, x, y, z);
@@ -64,7 +97,13 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 	    tessellator.setColorOpaque_F(1.0F, 1.0F, 1.0F);
 
 
+
+
 	    renderBlock(tessellator, x, y, z, block);*/
+
+
+
+
 
 
 
@@ -77,10 +116,14 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 	}
 
 
+
+
 	@Override
 	public boolean shouldRender3DInInventory(int modelId) {
 		return true;
 	}
+
+
 
 
 	@Override
@@ -88,6 +131,8 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 		// TODO Auto-generated method stub
 		return BerryRender.myRenderID;
 	}
+
+
 
 
 	private void renderBlock(Tessellator tess, double x, double y, double z, Block block){
@@ -98,12 +143,16 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 	    double maxV1 = (double)icon1.getMaxV();
 
 
+
+
 	    tess.setTranslation(x, y, z);
 	    tess.addVertexWithUV(0, 0, 0, maxU1, maxV1);
 	    tess.addVertexWithUV(0, 0.25, 0, maxU1, minV1);
 	    tess.addVertexWithUV(1, 0.25, 0, minU1, minV1);
 	    tess.addVertexWithUV(1, 0, 0, minU1, maxV1);
 	    tess.draw();
+
+
 
 
 	    tess.startDrawingQuads();
@@ -114,6 +163,8 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 	    tess.draw();
 
 
+
+
 	    tess.startDrawingQuads();
 	    tess.addVertexWithUV(0, 0, 1, minU1, maxV1);
 	    tess.addVertexWithUV(0, 0.25, 1, minU1, minV1);
@@ -122,17 +173,42 @@ public class BerryRender implements ISimpleBlockRenderingHandler {
 	    tess.draw();
 
 
+
+
 	    tess.startDrawingQuads();
 	    tess.addVertexWithUV(1, 0, 1, maxU1, maxV1);
 	    tess.addVertexWithUV(1, 0.25, 1, maxU1, minV1);
 	    tess.addVertexWithUV(1, 0.25, 0, minU1, minV1);
 	    tess.addVertexWithUV(1, 0, 0, minU1, maxV1);
+	    tess.draw();
+
+
+	    tess.startDrawingQuads();
+	    tess.addVertexWithUV(1, 0.25, 1, maxU1, maxV1);
+	    tess.addVertexWithUV(1, 0.25, 0, maxU1, minV1);
+	    tess.addVertexWithUV(0, 0.25, 0, minU1, minV1);
+	    tess.addVertexWithUV(0, 0.25, 1, minU1, maxV1);
+	    tess.draw();
+
+
+	    tess.startDrawingQuads();
+	    tess.addVertexWithUV(1, 0, 1, maxU1, maxV1);
+	    tess.addVertexWithUV(1, 0, 0, maxU1, minV1);
+	    tess.addVertexWithUV(0, 0, 0, minU1, minV1);
+	    tess.addVertexWithUV(0, 0, 1, minU1, maxV1);
+
+
+
+
+
 
 
 
 
 
 	}
+
+
 
 
 }
--- src/main/java/dmf444/ExtraFood/Common/blocks/StrawberryBush.java
+++ src/main/java/dmf444/ExtraFood/Common/blocks/StrawberryBush.java
@@ -1,6 +1,8 @@
 package dmf444.ExtraFood.Common.blocks;
 
+
 import java.util.Random;
+
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -10,6 +12,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
@@ -23,11 +26,13 @@ import dmf444.ExtraFood.Common.items.ItemLoader;
 import dmf444.ExtraFood.Core.EFTabs;
 import dmf444.ExtraFood.util.EFLog;
 
+
 public class StrawberryBush extends Block implements IGrowable {
-	
+
+
 	private static IIcon[] growingTextures;
 
-	
+
 	public StrawberryBush(Material material){
 		super(material);
 		float f = 0.5F;
@@ -47,6 +52,7 @@ public class StrawberryBush extends Block implements IGrowable {
 	                return this.growingTextures[1];
 	            }
 
+
 	            return this.growingTextures[0];
 	        }
 	        else
@@ -55,38 +61,85 @@ public class StrawberryBush extends Block implements IGrowable {
 	        }
 	}
 
+
 	@Override
 	public void updateTick(World world, int x, int y, int z, Random random) {
 	    {
 	        super.updateTick(world, x, y, z, random);
 
-	        if (world.getBlockLightValue(x, y + 1, z) >= 9)
-	        {
-	            int l = world.getBlockMetadata( x, y, z);
 
-	            if (l < 7)
-	            {
-	                float f = this.magicStuff(world,  x, y, z);
+//	        if (world.getBlockLightValue(x, y + 1, z) >= 9) 
+	        // im just gonna change this to my own code, okay?
+	        //NO IT'S NOT OK!!!! I WANT MY CODE
+	        //MOMMY- HE STOLE MY CODE!!!
+//	        {
+//	            int l = world.getBlockMetadata( x, y, z);
+//
+//	            if (l < 7)
+//	            {
+//	                float f = this.magicStuff(world,  x, y, z);
+//
+//	                if (random.nextInt((int)(25.0F / f) + 1) == 0)
+//	                {
+//	                    ++l;
+//	                    world.setBlockMetadataWithNotify(x, y, z, l, 2);
+//	                }
+//	            }
+//	        }
+	        if (world.getBlockLightValue(x, y + 1, z) >= 9) {
+	        	// Can survive, next!
+	        	int meta = world.getBlockMetadata(x, y, z);
 
-	                if (random.nextInt((int)(25.0F / f) + 1) == 0)
-	                {
-	                    ++l;
-	                    world.setBlockMetadataWithNotify(x, y, z, l, 2);
-	                }
-	            }
+
+	        	// Can grow more?
+	        	if (meta < 7){
+	        		// Yes, yes it can!
+	        		// Now, should I use my magical power to make it grow a bit?
+	        		// Well, first lets check if it is in nice rows:
+	        		float modifier = this.getGrowthModifierForBlock(world, x, y, z);
+	        		// Now, with the modifier in mind, check if we should add to the growth:
+	        		if (random.nextInt((int)(25.0 / modifier) + 1) == 0){
+	        			// Randomly give it a little boost:
+	        			if (random.nextInt(7) == 0) {meta += 2;}
+	        			else {meta += 1;}
+	        			world.setBlockMetadataWithNotify(x, y, z, meta, 2);
+	        		}
+
+
+
+
+	        	}
+	        	// No, lets go home now :(
 	        }
 	    }
 	}
-	
+
+
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float what, float these, float are) {
     	int meta = world.getBlockMetadata(x, y, z);
     	EFLog.info("Current Meta:" + meta);
+    	if (player.inventory.getCurrentItem() != null){
+    		ItemStack is = player.inventory.getCurrentItem();
+    		if (is.getItem() == Items.dye){
+    			if (is.getItemDamage() == 15){
+    				return false;
+    			}
+    		}
+    	}
     	switch (meta) {
     	case -1:
     			return false;
     	case 4: 
 			this.placeDuoInInv(player);
+			world.setBlockMetadataWithNotify(x, y, z, 0, 2);   		
+    			return true;
+    	case 5:
+    		this.placeDuoInInv(player);
+			world.setBlockMetadataWithNotify(x, y, z, 0, 2);   		
+    			return true;
+    	case 6:
+    		this.placeDuoInInv(player);
 			world.setBlockMetadataWithNotify(x, y, z, 0, 2);   		
     			return true;
     	case 7:
@@ -97,6 +150,7 @@ public class StrawberryBush extends Block implements IGrowable {
 		return false;
     }
 
+
 	private void placeDuoInInv(EntityPlayer player) {
 		if (player.inventory.getFirstEmptyStack() == -1){
 			player.inventory.addItemStackToInventory(new ItemStack(ItemLoader.strawberry, 2));
@@ -104,7 +158,8 @@ public class StrawberryBush extends Block implements IGrowable {
 			player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(ItemLoader.strawberry, 2));
 		}	
 	}
-	
+
+
 	private void placeInInv(EntityPlayer player) {
 		player.inventory.addItemStackToInventory(new ItemStack(ItemLoader.strawberry, 4));	
 	}
@@ -113,9 +168,11 @@ public class StrawberryBush extends Block implements IGrowable {
 	    {
 	        this.growingTextures = new IIcon[3];
 
+
 	            this.growingTextures[0] = iiconr.registerIcon("extrafood:berries_stage_0");
 	            this.growingTextures[1] = iiconr.registerIcon("extrafood:strawberry_stage_1");
 	            this.growingTextures[2] = iiconr.registerIcon("extrafood:strawberry_stage_2");
+
 
 	    }
 	    public int getRenderType()
@@ -143,24 +200,39 @@ public class StrawberryBush extends Block implements IGrowable {
 		public void func_149853_b(World p_149853_1_, Random p_149853_2_, int p_149853_3_, int p_149853_4_, int p_149853_5_) {
 			this.onBonemealEvent(p_149853_1_, p_149853_3_, p_149853_4_, p_149853_5_);			
 		}
-		
+
+
 	    public void onBonemealEvent(World world, int x, int y, int z)
 	    {
-	        int l = world.getBlockMetadata(x, y, z) + MathHelper.getRandomIntegerInRange(world.rand, 2, 5);
+	        int meta = world.getBlockMetadata(x, y, z) + 1;
 
-	        if (l > 7)
-	        {
-	            l = 7;
+
+	        boolean randfact = world.rand.nextInt(3) <= 1;
+	        if (meta < 7){
+		        if ( randfact && meta == 6){
+		        	meta += 1;
+		        }
+		        else if (randfact && meta >= 4){
+		        	meta += 2;
+		        }
+		        else if (randfact && meta < 4){
+		        	meta += 3;
+		        }
+		        else {
+		        	meta += 1;
+		        }
 	        }
 
-	        world.setBlockMetadataWithNotify(x, y, z, l++, 2);
+
+	        world.setBlockMetadataWithNotify(x, y, z, meta, 2);
 	    }
 	    public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
 	    {
 	        return null;
 	    }
-	    
-	    private float magicStuff(World world, int x, int y, int z)
+
+
+	    private float getGrowthModifierForBlock(World world, int x, int y, int z)
 	    {
 	        float f = 1.0F;
 	        Block block = world.getBlock(x, y, z - 1);
@@ -175,15 +247,18 @@ public class StrawberryBush extends Block implements IGrowable {
 	        boolean flag1 = block == this || block1 == this;
 	        boolean flag2 = block4 == this || block5 == this || block6 == this || block7 == this;
 
+
 	        for (int l = x - 1; l <= x + 1; ++l)
 	        {
 	            for (int i1 = z - 1; i1 <= z + 1; ++i1)
 	            {
 	                float f1 = 0.0F;
 
+
 	                if (world.getBlock(l, y - 1, i1) == Blocks.dirt || world.getBlock(l, y - 1, i1) == Blocks.grass)
 	                {
 	                    f1 = 1.0F;
+
 
 	                    if (world.getBlock(l, y - 1, i1).isFertile(world, l, y - 1, i1))
 	                    {
@@ -191,19 +266,23 @@ public class StrawberryBush extends Block implements IGrowable {
 	                    }
 	                }
 
+
 	                if (l != x || i1 != z)
 	                {
 	                    f1 /= 4.0F;
 	                }
 
+
 	                f += f1;
 	            }
 	        }
+
 
 	        if (flag2 || flag && flag1)
 	        {
 	            f /= 2.0F;
 	        }
+
 
 	        return f;
 	    }
