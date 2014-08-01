1d7f647ed13029b594767f27c237feee0bdc4177
mincrmatt12 <matthewmirvish@hotmail.com>
2014-08-01 09:00:36 -0400

    Actual push for the new Renderer
    YAY!!!!!!!!!

--- src/main/java/dmf444/ExtraFood/Common/blocks/guis/RenderInstruction.java
+++ src/main/java/dmf444/ExtraFood/Common/blocks/guis/RenderInstruction.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package dmf444.ExtraFood.Common.blocks.guis;
 
 public class RenderInstruction {
 
 }
-=======
-package dmf444.ExtraFood.Common.blocks.guis;
-
-public class RenderInstruction {
-
-}
->>>>>>> origin/master
